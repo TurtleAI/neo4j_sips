11ce7c7b6a207f629ab986ca5e9e7fc267332766
Venkat Dinavahi <vendiddy@gmail.com>
2016-03-20 17:09:27 -0400

    pull auth token on demand

--- lib/neo4j_sips/http.ex
+++ lib/neo4j_sips/http.ex
@@ -6,40 +6,36 @@ defmodule Neo4j.Sips.Http do
   """
   use HTTPoison.Base
 
-  token_auth = nil
-
-  IO.inspect Neo4j.Sips.config
-
-  if basic_auth = Neo4j.Sips.config[:basic_auth] do
-    username = basic_auth[:username]
-    password = basic_auth[:password]
-    token_auth = Base.encode64("#{username}:#{password}")
-  end
-
-  if Neo4j.Sips.config[:token_auth] != nil do
-    token_auth = Neo4j.Sips.config[:token_auth]
-    def auth_token, do: unquote(Macro.escape(token_auth))
-  else
-    def auth_token, do: nil
-  end
-
-  @headers [
+  @base_headers [
     "Accept": "application/json; charset=UTF-8",
     "Content-Type": "application/json; charset=UTF-8",
     "User-Agent": "Neo4j.Sips client",
-    "X-Stream": "true",
-    "Authorization": "Basic #{token_auth}"
+    "X-Stream": "true"
   ]
-
-  IO.inspect @headers
 
   @doc false
   @spec process_request_headers(map) :: map
   def process_request_headers(header) do
-    @headers ++ header
+    headers ++ header
   end
 
-  def headers do
-    @headers
+  defp headers do
+    @base_headers ++ ["Authorization": "Basic #{token_auth}"]
   end
+
+  defp token_auth do
+    basic_auth = Neo4j.Sips.config[:basic_auth]
+    token_auth = Neo4j.Sips.config[:token_auth]
+
+    cond do
+      token_auth != nil ->
+        token_auth
+
+      basic_auth != nil ->
+        username = basic_auth[:username]
+        password = basic_auth[:password]
+        Base.encode64("#{username}:#{password}")
+    end
+  end
+
 end
